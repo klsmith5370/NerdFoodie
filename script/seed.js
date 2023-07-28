23f669f817c4809143d047b3fba04659c0db4e1e
Kourtney Smith <klsmith5370@gmail.com>
2023-07-28 14:30:41 -0400

    Created base for seed file and database associations

---
+++ script/seed.js
@@ -0,0 +1,43 @@
+'use strict'
+
+const { db, models: { } } = require('../server/db/index')
+
+async function seed() {
+    await db.sync({ force: true }) // clears db and matches models to tables
+    console.log('db synced!')
+
+    // creates users
+
+    // creates food recipes
+
+    console.log(`seeded ${users.length} users`)
+    console.log(`seeded ${characterRecipes.length} recipes`)
+    console.log(`seeded successfully`)
+    return {
+      users: {
+        kls5370: users[0],
+        yuna: users[1]
+      }
+    }
+}
+
+async function runSeed() {
+    console.log('seeding...')
+    try {
+      await seed()
+    } catch (err) {
+      console.error(err)
+      process.exitCode = 1
+    } finally {
+      console.log('closing db connection')
+      await db.close()
+      console.log('db connection closed')
+    }
+}
+
+if (module === require.main) {
+    runSeed()
+}
+
+module.exports = seed
+
---
+++ server/db/index.js
@@ -0,0 +1,13 @@
+const db = require('./db')
+
+const User = require('./models/User')
+
+
+// Associations go here!
+
+module.exports = {
+  db,
+  models: {
+
+  },
+}
