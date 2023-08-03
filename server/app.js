1f672bc2eb3a5242b987f665bf01c3016c0c9687
Kourtney Smith <klsmith5370@gmail.com>
2023-08-03 14:43:46 -0400

    Working on getting set up with GraphQL with Express and Sequelize

--- server/app.js
+++ server/app.js
@@ -1,9 +1,16 @@
 const path = require('path')
 const express = require('express')
 const morgan = require('morgan')
+const { graphqlHTTP } = require('express-graphql')
+const { buildSchema } = require('graphql')
 const app = express()
 
 module.exports = app
+
+// resolvers
+const root = {
+  
+}
 
 // logging middleware
 app.use(morgan('dev'))
@@ -12,30 +19,40 @@ app.use(morgan('dev'))
 app.use(express.json())
 
 // auth and api routes
-app.use('/auth', require('./auth'))
-app.use('/api', require('./api'))
+// app.use('/auth', require('./auth'))
+// app.use('/api', require('./api'))
 
 
-app.get('/', (req, res)=> res.sendFile(path.join(__dirname, '..', 'public/index.html')));
+// app.get('/', (req, res)=> res.sendFile(path.join(__dirname, '..', 'public/index.html')));
 
-// static file-serving middleware
-app.use(express.static(path.join(__dirname, '..', 'public')))
+// // static file-serving middleware
+// app.use(express.static(path.join(__dirname, '..', 'public')))
 
-// any remaining requests with an extension (.js, .css, etc.) send 404
-app.use((req, res, next) => {
-  if (path.extname(req.path).length) {
-    const err = new Error('Not found')
-    err.status = 404
-    next(err)
-  } else {
-    next()
-  }
-})
+// // any remaining requests with an extension (.js, .css, etc.) send 404
+// app.use((req, res, next) => {
+//   if (path.extname(req.path).length) {
+//     const err = new Error('Not found')
+//     err.status = 404
+//     next(err)
+//   } else {
+//     next()
+//   }
+// })
 
-// sends index.html
-app.use('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'public/index.html'));
-})
+// // sends index.html
+// app.use('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '..', 'public/index.html'));
+// })
+
+// GraphQL middleware
+app.use(
+    'graphql',
+    graphqlHTTP({
+        schema: schema,
+        rootValue: root,
+        graphiql: true,
+    })
+)
 
 // error handling endware
 app.use((err, req, res, next) => {
