cf76374d04320d6e911af75b5eb3830766b961b4
Kourtney Smith <klsmith5370@gmail.com>
2023-08-04 15:21:53 -0400

    added schemas and resolvers into a graphql folder

---
+++ server/graphql/resolvers.js
@@ -0,0 +1,45 @@
+const { User, CharacterRecipe } = require('../db/models');
+
+const root = {
+  users: async () => {
+    try {
+      const users = await User.findAll();
+      return users;
+    } catch (error) {
+      throw new Error('Error fetching users:', error);
+    }
+  },
+  user: async ({ id }) => {
+    try {
+      const user = await User.findByPk(id);
+      return user;
+    } catch (error) {
+      throw new Error('Error fetching user:', error);
+    }
+  },
+  characterRecipes: async () => {
+    try {
+      const recipes = await CharacterRecipe.findAll();
+      return recipes;
+    } catch (error) {
+      throw new Error('Error fetching character recipes:', error);
+    }
+  },
+  characterRecipe: async ({ id }) => {
+    try {
+      const recipe = await CharacterRecipe.findByPk(id);
+      return recipe;
+    } catch (error) {
+      throw new Error('Error fetching character recipe:', error);
+    }
+  },
+  createUser: async ({ firstName, lastName, username, password, email, bio, image }) => {
+    try {
+      const user = await User.create({ firstName, lastName, username, password, email, bio, image });
+      return user;
+    } catch (error) {
+      throw new Error('Error creating user:', error);
+    }
+  },
+  // Add more resolver functions for mutations as needed
+};
---
+++ server/graphql/schema.js
@@ -0,0 +1,36 @@
+const schema = buildSchema(`
+  type User {
+    id: ID!
+    firstName: String!
+    lastName: String!
+    username: String!
+    email: String!
+    bio: String
+    image: String
+  }
+
+  type CharacterRecipe {
+    id: ID!
+    characterName: String!
+    recipeName: String!
+    recipeIngredients: String
+    recipeInstructions: String
+    recipeDescription: String
+    recipeCategory: [String]
+    characterImage: String
+  }
+
+  type Query {
+    users: [User!]!
+    user(id: ID!): User
+    characterRecipes: [CharacterRecipe!]!
+    characterRecipe(id: ID!): CharacterRecipe
+  }
+
+  type Mutation {
+    createUser(firstName: String!, lastName: String!, username: String!, password: String!, email: String!, bio: String, image: String): User
+    // Add more mutation functions as needed
+  }
+`);
+
+module.exports = schema;
