2f755f40ec02b14300ea95252c51822a2f7884dc
Kourtney Smith <klsmith5370@gmail.com>
2024-06-05 15:43:03 -0400

    Commented out graphQL resolvers and schemas (try to use in a future update)

--- server/graphql/schema.js
+++ server/graphql/schema.js
@@ -1,45 +1,45 @@
-const { buildSchema } = require('graphql');
+// const { buildSchema } = require('graphql');
 
-const schema = buildSchema(`
-  type User {
-    id: ID!
-    firstName: String!
-    lastName: String!
-    username: String!
-    email: String!
-    bio: String
-    image: String
-  }
+// const schema = buildSchema(`
+//   type User {
+//     id: ID!
+//     firstName: String!
+//     lastName: String!
+//     username: String!
+//     email: String!
+//     bio: String
+//     image: String
+//   }
 
-  type CharacterRecipe {
-    id: ID!
-    characterName: String!
-    recipeName: String!
-    recipeIngredients: String
-    recipeInstructions: String
-    recipeDescription: String
-    recipeCategory: [String]
-    characterImage: String
-  }
+//   type CharacterRecipe {
+//     id: ID!
+//     characterName: String!
+//     recipeName: String!
+//     recipeIngredients: String
+//     recipeInstructions: String
+//     recipeDescription: String
+//     recipeCategory: [String]
+//     characterImage: String
+//   }
 
-  type Query {
-    users: [User!]!
-    user(id: ID!): User
-    characterRecipes: [CharacterRecipe!]!
-    characterRecipe(id: ID!): CharacterRecipe
-  }
+//   type Query {
+//     users: [User!]!
+//     user(id: ID!): User
+//     characterRecipes: [CharacterRecipe!]!
+//     characterRecipe(id: ID!): CharacterRecipe
+//   }
 
-  type Mutation {
-    createUser(
-      firstName: String!, 
-      lastName: String!, 
-      username: String!, 
-      password: String!, 
-      email: String!, 
-      bio: String, 
-      image: String): User
-    // Add more mutation functions as needed
-  }
-`)
+//   type Mutation {
+//     createUser(
+//       firstName: String!, 
+//       lastName: String!, 
+//       username: String!, 
+//       password: String!, 
+//       email: String!, 
+//       bio: String, 
+//       image: String): User
+//     // Add more mutation functions as needed
+//   }
+// `)
 
-module.exports = schema
+// module.exports = schema
